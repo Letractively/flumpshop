ba242abd363fb6e0219a0b5f750fa197d45c451e
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-01-19 23:42:13 +0000

    Fixed Issue 19

--- trunk/admin/endpoints/create/newCategory.php
+++ trunk/admin/endpoints/create/newCategory.php
@@ -6,7 +6,7 @@ require_once dirname(__FILE__)."/../../../preload.php";
 <legend>Add Category</legend>
 <form action="./endpoints/process/insertCategory.php" method="post" class="ui-widget-content" onsubmit="if ($(this).valid()) {$(this).ajaxSubmit({target: '#adminContent'});} else {$('#submit').removeClass('ui-state-disabled').val('Save');} return false;">
 <label for="name">Name: </label><input type="text" maxlength="255" name="name" id="name" class="ui-widget-content ui-state-default required" /><br />
-<label for="description">Description: </label><br /><textarea rows="4" cols="45" name="description" id="description" class="ui-widget-content ui-state-default" /><br />
+<label for="description">Description: </label><br /><textarea rows="4" cols="45" name="description" id="description" class="ui-widget-content ui-state-default"></textarea><br />
 <label for="parent">Parent: </label>
 <select name="parent" id="parent" class="ui-widget-content">
 	<option value="0" selected="selected">No Parent</option>
