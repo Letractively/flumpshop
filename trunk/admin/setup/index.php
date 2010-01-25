05252ed598b2cd00b9b2838187535e10335d4acb
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-01-25 12:07:33 +0000

    Fixed home page navigation in IE7 with low 1024x768 resolution

--- trunk/admin/setup/index.php
+++ trunk/admin/setup/index.php
@@ -211,7 +211,7 @@
 						$config->setNode("site", "shopMode", false, "Shop Mode");
 						$config->setNode("site", "homeTab", true, "Home Tab");
 						$config->setNode("site", "sendFeedback", true, "Send Feedback");
-						$config->setNode("site", "version", "0.7.176", "Version");
+						$config->setNode("site", "version", "0.7.185", "Version");
 						
 						//Paths and Directories Tree
 						$config->addTree("paths", "Site Paths and Directories");
