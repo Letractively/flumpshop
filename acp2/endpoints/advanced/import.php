96cc452a2fcc3dfe96dd1a9e78ad99308685439e
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-01-30 23:23:52 +0000

    Moved to admin

--- acp2/endpoints/advanced/import.php
+++
@@ -1,7 +0,0 @@
-This feature uses a previously exported copy of my database to fix altered or missing data. However, I'll write over any conflicting data I find whilst updating the database.
-<form action="./endpoints/process/doImport.php" method="post" enctype="multipart/form-data" onsubmit="$(this).ajaxSubmit({target: '#adminContent'}); $('#submitButton').attr('disabled',true).addClass('ui-state-disabled').val('Importing Data. This may take some time.'); return false;">
-	<input type="file" name="file" id="file" /><br />
-    <input type="checkbox" name="includeConf" id="includeConf" checked="checked" />
-    <label for='includeConf'>Replace the Configuration Object</label><br />
-    <input type="submit" id="submitButton" value="Import" class="ui-widget-content" />
-</form>
--- acp2/endpoints/process/bugresolved.php
+++
@@ -1,8 +0,0 @@
-<?php
-$ajaxProvider = true;
-require_once dirname(__FILE__)."/../../../preload.php";
-
-$dbConn->query("UPDATE `bugs` SET `resolved` = 1 WHERE id = ".$_GET['id']." LIMIT 1");
-
-header("Location: ../advanced/bugs.php");
-?>
