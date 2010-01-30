3dd7ded96b7f571601f2bf382913c8da956ef01e
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-01-30 23:23:52 +0000

    Moved to admin

--- trunk/acp2/endpoints/process/disableItem.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once dirname(__FILE__)."/../../../preload.php";
-if (!isset($_SESSION['adminAuth']) || !$_SESSION['adminAuth']) die($config->getNode('messages','adminDenied'));
-
-$id = intval($_GET['id']);
-
-$dbConn->query("UPDATE `products` SET active=0 WHERE id='$id' LIMIT 1");
-?><div class="ui-state-highlight"><span class="ui-icon ui-icon-circle-check"></span>Product Disabled. It will no longer appear in any public category lists.</div>
--- trunk/acp2/endpoints/process/installWidget.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once dirname(__FILE__)."/../../../preload.php";
-$widget = unserialize(base64_decode(file_get_contents($_FILES['widget']['tmp_name'])));
-
-//Import Configuration Variables
-?>
