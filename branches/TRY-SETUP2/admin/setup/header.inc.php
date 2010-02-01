17a0e8cd38a50a7a98856c8ebf175995421ea1d7
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-02-01 17:52:34 +0000

    Added initial layout

---
+++ branches/TRY-SETUP2/admin/setup/header.inc.php
@@ -0,0 +1,3 @@
+<html>
+<head><link href="../../style-main.css" rel="stylesheet" type="text/css" /><link href="../../jqueryui.css" rel="stylesheet" type="text/css" /><script type="text/javascript" language="javascript" src="../../../js/jquery.js"></script></head>
+<body>
