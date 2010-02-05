2fe07bcdae4066e579cab434c76ec3a86512937e
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-02-05 22:43:32 +0000

    Adds file to fix configuration issues with new category layout

---
+++ admin/fix-0.9.251.php
@@ -0,0 +1,6 @@
+<?php
+require_once dirname(__FILE__)."/../preload.php";
+$config->setNode('viewItem', 'catCols', 2, 'Category View Columns');
+$config->setNode('viewItem', 'catTextPos', 'bottom', 'Category Text Position');
+$config->setNode('viewItem', 'catChars', 50, 'Category Text Length');
+?>
