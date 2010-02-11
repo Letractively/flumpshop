9de0cd358f991dd96aea86bd335051c935747bc3
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-02-11 22:32:27 +0000

    Fixes issue 48

--- includes/Paginator.class.php
+++ includes/Paginator.class.php
@@ -1,7 +1,7 @@
 <?php
 class Paginator {
 	
-	function paginate($cpage,$perPage,$items,$urlprefix,$ajax = NULL) {
+	function paginate($cpage,$perPage,$items,$urlprefix,$ajax = NULL,$rewrite = false) {
 		global $config;
 		
 		$pages = ceil($items/$perPage);
@@ -10,9 +10,15 @@ class Paginator {
 		$return = "<div class='ui-state-default' style='text-align: center;'>";
 		if ($cpage != 1) {
 			if ($ajax != NULL) {
+				//Ajax mode
 				$return .= "<a href='javascript:void(0);' onclick='$(\"#".$ajax."\").html(loadMsg(\"Loading Content...\")).load(\"".$urlprefix.$join."page=1\");'>".$config->getNode('messages','firstPage')."</a>&nbsp;";
 				$return .= "<a href='javascript:void(0);' onclick='$(\"#{$ajax}\").html(loadMsg(\"Loading Content...\")).load(\"".$urlprefix.$join."page=".($cpage-1)."\");'>".$config->getNode('messages','previousPage')."</a>&nbsp;";
+			} elseif ($rewrite) {
+				//URL Rewrite Mode
+				$return .= "<a href='".$urlprefix."1'>".$config->getNode('messages','firstPage')."</a>&nbsp;";
+				$return .= "<a href='".$urlprefix.($cpage-1)."'>".$config->getNode('messages','previousPage')."</a>&nbsp;";
 			} else {
+				//Normal
 				$return .= "<a href='".$urlprefix.$join."page=1'>".$config->getNode('messages','firstPage')."</a>&nbsp;";
 				$return .= "<a href='".$urlprefix.$join."page=".($cpage-1)."'>".$config->getNode('messages','previousPage')."</a>&nbsp;";
 			}
@@ -21,8 +27,13 @@ class Paginator {
 		for ($i = $cpage-10; $i < $cpage; $i++) {
 			if ($i > 0) {
 				if ($ajax != NULL) {
+					//AJAX
 					$return .= "<a href='javascript:void(0);' onclick='$(\"#$ajax\").html(loadMsg(\"Loading Content...\")).load(\"".$urlprefix.$join."page=$i\");'>".$i."</a>&nbsp;";
+				} elseif ($rewrite) {
+					//Rewrite
+					$return .= "<a href='".$urlprefix."$i'>".$i."</a>&nbsp;";
 				} else {
+					//Normal
 					$return .= "<a href='".$urlprefix.$join."page=$i'>".$i."</a>&nbsp;";
 				}
 			}
@@ -32,17 +43,28 @@ class Paginator {
 		
 		for ($i = $cpage+1; $i <= $pages && $i <$cpage+10; $i++) {
 			if ($ajax != NULL) {
+				//Ajax
 				$return .= "<a href='javascript:void(0);' onclick='$(\"#$ajax\").html(loadMsg(\"Loading Content...\")).load(\"".$urlprefix.$join."page=$i\");'>".$i."</a>&nbsp;";
+			} elseif ($rewrite) {
+				//Rewrite
+				$return .= "<a href='".$urlprefix.$i."'>".$i."</a>&nbsp;";
 			} else {
+				//Normal
 				$return .= "<a href='".$urlprefix.$join."page=$i'>".$i."</a>&nbsp;";
 			}
 		}
 		
 		if ($cpage != $pages) {
 			if ($ajax != NULL) {
+				//Ajax
 				$return .= "<a href='javascript:void(0);' onclick='$(\"#$ajax\").html(loadMsg(\"Loading Content...\")).load(\"".$urlprefix.$join."page=".($cpage+1)."\");'>".$config->getNode('messages','nextPage')."</a>&nbsp;";
 				$return .= "<a href='javascript:void(0);' onclick='$(\"#$ajax\").html(loadMsg(\"Loading Content...\")).load(\"".$urlprefix.$join."page=$pages\");'>".$config->getNode('messages','lastPage')."</a>&nbsp;";
+			} elseif ($rewrite) {
+				//Rewrite
+				$return .= "<a href='".$urlprefix.($cpage+1)."'>".$config->getNode('messages','nextPage')."</a>&nbsp;";
+				$return .= "<a href='".$urlprefix.$pages."'>".$config->getNode('messages','lastPage')."</a>&nbsp;";
 			} else {
+				//Normal
 				$return .= "<a href='".$urlprefix.$join."page=".($cpage+1)."'>".$config->getNode('messages','nextPage')."</a>&nbsp;";
 				$return .= "<a href='".$urlprefix.$join."page=$pages'>".$config->getNode('messages','lastPage')."</a>&nbsp;";
 			}
