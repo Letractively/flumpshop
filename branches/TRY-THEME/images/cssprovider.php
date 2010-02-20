c789a593db7225df283d4e03ee4bb79fcd1fa490
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-02-20 22:08:47 +0000

    Deleted some obsolete files, and optimised image providers

--- branches/TRY-THEME/images/cssprovider.php
+++ branches/TRY-THEME/images/cssprovider.php
@@ -1,5 +1,6 @@
 <?php
-require_once "../preload.php";
+//die();
+require_once "../includes/vars.inc.php";
 header("Content-Type: image/png");
 
 echo file_get_contents($config->getNode('paths','offlineDir')."/themes/".$_GET['theme']."/images/".$_GET['image'].".png");
