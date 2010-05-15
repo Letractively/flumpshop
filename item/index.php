93bb5dbe63867d35318c9f144f6b00ccaff8b62d
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-05-15 16:15:05 +0000

    Experimetal caching support has been added, and is used on item pages and the navigation bar

--- item/index.php
+++ item/index.php
@@ -6,33 +6,28 @@ require_once dirname(__FILE__)."/../header.php";
 
 ob_start(); //Template Buffer
 
+if ($config->isNode('cache','item'.$_GET['id'])) {
+	echo $config->getNode('cache','item'.$_GET['id']);
+} else {
+
     $category = new Category($item->getCategory());
     echo "<a href='".$config->getNode('paths','root')."'>Home</a> -> ".$category->getBreadcrumb()." -> ".$item->getName();
 	echo '<div id="notice"></div>';
 	echo $item->getDetails("FULL");
+		
+	?><script type="text/javascript">
+	document.id = <?php echo intval($_GET['id']); ?>;
+	//Full size image dialog
+	function openImageViewer(imageID) {
+		$("#dialog").html("<img src='<?php echo $config->getNode('paths','root');?>/item/imageProvider.php?id="+document.id+"&amp;image="+imageID+"&amp;size=full' style='max-width: 800px; max-height: 500px;' />")
+		document.getElementById("dialog").title = "View Image";
+		$("#dialog").dialog({height: "auto", width: "auto", resizable: true, position: "left", buttons: {"Close": function() {$(this).dialog("destroy");}}});
+	}
+	</script><?php
 	
-?><script type="text/javascript">
-document.id = <?php echo intval($_GET['id']); ?>;
-function reduceItem() {
-	$("#dialog").html("<form action='<?php echo $config->getNode('paths','root');?>/item/createReduction.php' method='post' name='reducePriceForm' id='reducePriceForm'><table><tr><td><label for='reducedPrice'>Reduced Price:</label></td><td><input type='text' name='reducedPrice' id='reducedPrice' style='width: 110px;' value='"+$('#itemPrice').html()+"' /></td></tr><tr><td><label for='validDate'>Valid From:</label></td><td><input type='text' name='validDate' id='validDate' style='width: 110px;' /></td></tr><tr><td><label for='expiresDate'>Expires:</label></td><td><input type='text' name='expiresDate' id='expiresDate' style='width: 110px;' /></td></tr></table><input type='hidden' name='itemID' id='itemID' value='<?php echo $item->getID();?>' /></form>");
-	$("#validDate").datepicker({minDate: +0, dateFormat: "dd/mm/yy", showButtonPanel: true});
-	$("#expiresDate").datepicker({minDate: +0, dateFormat: "dd/mm/yy", showButtonPanel: true});
-	$("#ui-datepicker-div").css("z-index",2006); 
-	document.getElementById("dialog").title = "Create Offer";
-	$("#dialog").dialog({buttons: {
-							"Create Offer": function() {$('#reducePriceForm').submit();},
-							Cancel: function() {$(this).dialog('destroy');}
-							}
-						});
+	//Cache the data now
+	$config->setNode("cache","item".$item->getID(),ob_get_contents());
 }
-//Full size image dialog
-function openImageViewer(imageID) {
-	$("#dialog").html("<img src='<?php echo $config->getNode('paths','root');?>/item/imageProvider.php?id="+document.id+"&amp;image="+imageID+"&amp;size=full' style='max-width: 800px; max-height: 500px;' />")
-	document.getElementById("dialog").title = "View Image";
-	$("#dialog").dialog({height: "auto", width: "auto", resizable: true, position: "left", buttons: {"Close": function() {$(this).dialog("destroy");}}});
-
-}
-</script><?php
 
 templateContent($_GET['id']);
 
