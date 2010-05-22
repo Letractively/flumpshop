bdbea5a05f106def34701a1ab9d6a9a32e36a5e0
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-05-22 23:18:49 +0000

    Now processes feature units in the create form properly

--- trunk/admin/endpoints/features/processCreateForm.php
+++ trunk/admin/endpoints/features/processCreateForm.php
@@ -9,6 +9,11 @@ $default = htmlentities($_POST['default'],ENT_QUOTES);
 
 if ($dbConn->query("INSERT INTO `compare_features` (feature_name,data_type,default_value) VALUES ('$name','$datatype','$default')")) {
 	echo "<div class='ui-state-highlight'><span class='ui-icon ui-icon-circle-check'></span>Feature $name has been added succesfully.</div>";
+	//Create Units
+	for ($i = 0; isset($_POST['unitMultiple'.$i]);$i++) {
+		$newId = $dbConn->insert_id();
+		$dbConn->query("INSERT INTO feature_units (feature_id,multiple,unit) VALUES ($newId,".intval($_POST['unitMultiple'.$i]).",'".str_replace("'","''",$_POST['unitSuffix'.$i])."')");
+	}
 } else {
 	echo "<div class='ui-state-alert'><span class='ui-icon ui-icon-alert'></span>Failed to create feature attribute. Please contact your system administrator.</div>";
 }
