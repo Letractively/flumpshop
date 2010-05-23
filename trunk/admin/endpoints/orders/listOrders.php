75d04b9a03708107258661733981c79c124a448b
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-05-23 16:51:21 +0000

    Implemented basic order tracking, and more ACP permissions

--- trunk/admin/endpoints/orders/listOrders.php
+++ trunk/admin/endpoints/orders/listOrders.php
@@ -5,8 +5,23 @@ $filter = $_GET['filter'];
 $query = "SELECT * FROM `orders` WHERE false";
 
 switch ($filter) {
+	//All active orders
 	case "active":
 		$orderStats = array_keys($config->getNodes('orderstatus'));
+		foreach ($orderStats as $status) { //Iterate through order statuses
+			if (!is_int($status)) continue;
+			$array = $config->getNode('orderstatus',$status);
+			if ($array['active'] == true) { //true means that the status represents and open order
+				$query .= " OR status='".$status."'";
+			}
+		}
+		$query.= " ORDER BY status ASC";
+		echo "<div class='ui-widget'><div class='ui-widget-header'>Active Orders</div><div class='ui-widget-content'>";
+		break;
+	//All active orders that are not assigned to a user
+	case "unassigned":
+		$orderStats = array_keys($config->getNodes('orderstatus'));
+		$query .= " OR (false";
 		foreach ($orderStats as $status) {
 			if (!is_int($status)) continue;
 			$array = $config->getNode('orderstatus',$status);
@@ -14,9 +29,11 @@ switch ($filter) {
 				$query .= " OR status='".$status."'";
 			}
 		}
-		$query.= " ORDER BY status ASC";
-		echo "<div class='ui-widget'><div class='ui-widget-header'>Active Orders</div><div class='ui-widget-content'>";
+		$query .= ")";
+		$query.= " AND assignedTo=0";
+		echo "<div class='ui-widget'><div class='ui-widget-header'>Unassigned Orders</div><div class='ui-widget-content'>";
 		break;
+	//All Closed Orders
 	case "closed":
 		$orderStats = array_keys($config->getNodes('orderstatus'));
 		foreach ($orderStats as $status) {
@@ -29,6 +46,37 @@ switch ($filter) {
 		$query.= " ORDER BY status ASC";
 		echo "<div class='ui-widget'><div class='ui-widget-header'>Closed Orders</div><div class='ui-widget-content'>";
 		break;
+	//Open orders assigned to a given user
+	case "assigned":
+		$orderStats = array_keys($config->getNodes('orderstatus'));
+		$query .= " OR (false";
+		foreach ($orderStats as $status) {
+			if (!is_int($status)) continue;
+			$array = $config->getNode('orderstatus',$status);
+			if ($array['active'] == true) {
+				$query .= " OR status='".$status."'";
+			}
+		}
+		$query .= ")";
+		$query.= " AND assignedTo=".intval($_GET['id']);
+		echo "<div class='ui-widget'><div class='ui-widget-header'>Assigned Orders</div><div class='ui-widget-content'>";
+		break;
+	//Orders that are assigned to the given user and closed
+	case "closed_assigned":
+		$orderStats = array_keys($config->getNodes('orderstatus'));
+		$query .= " OR (false";
+		foreach ($orderStats as $status) {
+			if (!is_int($status)) continue;
+			$array = $config->getNode('orderstatus',$status);
+			if ($array['active'] == false) {
+				$query .= " OR status='".$status."'";
+			}
+		}
+		$query .= ")";
+		$query.= " AND assignedTo=".intval($_GET['id']);
+		echo "<div class='ui-widget'><div class='ui-widget-header'>Closed Orders</div><div class='ui-widget-content'>";
+		break;
+	//Orders made by a given customer
 	case "customer":
 		$orderStats = array_keys($config->getNodes('orderstatus'));
 		$customer = intval($_GET['id']);
@@ -44,7 +92,7 @@ if ($dbConn->rows($orders) == 0) {
 }
 while ($order = $dbConn->fetch($orders)) {
 	$status = $config->getNode('orderstatus',$order['status']);
-	echo "<a href='javascript:void(0);' onclick=\"loader(loadMsg('Loading Content...'));$(body).load('order.php?id=".$order['id']."',{},function() {\$('#".$order['id']."').editable('./endpoints/process/updateStatus.php',{loadurl: 'statuses.php',type: 'select',submit: 'Save'})});\">Order #".$order['id']." (".$status['name'].")</a><br />";
+	echo "<a href='order.php?id=".$order['id']."'>Order #".$order['id']." (".$status['name'].")</a><br />";
 }
 
 echo "</div></div>";
--- trunk/admin/endpoints/orders/order.php
+++ trunk/admin/endpoints/orders/order.php
@@ -1,8 +1,6 @@
 <?php
 $USR_REQUIREMENT = "can_view_orders";
-$ajaxProvider = true;
-require_once dirname(__FILE__)."/../../../preload.php";
-if (!isset($_SESSION['adminAuth']) || $_SESSION['adminAuth'] == false) die($config->getNode('messages','adminDenied'));
+require_once "../header.php";
 
 $order = intval($_GET['id']);
 
@@ -15,10 +13,34 @@ if (!$order or $dbConn->rows($order) == 0) {
 $order = $dbConn->fetch($order);
 $status = $config->getNode('orderstatus',$order['status']);
 ?>
-<div class="ui-widget">
+<div id="saverTxt"></div>
+<div class="ui-widget" id="orderData">
 	<div class="ui-widget-header">Details for Order #<?php echo $order['id'];?></div>
-	<div class='ui-state-highlight'><span class='ui-icon ui-icon-flag'></span>Status: <span id="<?php echo $order['id']; ?>" style="display:inline;"><?php echo $status['name']; ?></span></div>
-    <div class="ui-widget-content">Customer ID: <?php echo $order['customer'];?> <a href='javascript:void(0);' onclick="loader(loadMsg('Loading Content...'));window.location = 'customer.php?id=<?php echo $order['customer']; ?>;">View Details</a><br />
+	<?php
+	if ($order['assignedTo'] == 0) {
+		?>
+		<div class="ui-state-highlight"><span class="ui-icon ui-icon-tag"></span>This order isn't assigned to anyone yet. <a href='assignOrder.php?oid=<?php echo $order['id'];?>&uid=<?php echo $acp_uid;?>'>Accept this order</a></div>
+		<?php
+	} elseif ($order['assignedTo'] == $acp_uid) {
+		?>
+		<div class="ui-state-highlight"><span class="ui-icon ui-icon-tag"></span>This order is assigned to you</div>
+		<?php
+	}
+	?>
+	<div class='ui-state-highlight'><span class='ui-icon ui-icon-flag'></span>Status: 
+	<select name="orderStatus" style="display:inline;" onchange="updateStatus(this.value);">
+	<option value="<?php echo $order['status'];?>"><?php echo $status['name']; ?></option>
+	<option disabled="disabled">-----</option>
+	<?php
+	$orderStats = array_keys($config->getNodes('orderstatus'));
+	foreach ($orderStats as $status) {
+		if (!is_int($status)) continue;
+		$array = $config->getNode('orderstatus',$status);
+		echo "<option value='".$status."'>".$array['name']."</option>";
+	}
+	?>
+	</select></div>
+    <div class="ui-widget-content">Customer ID: <?php echo $order['customer'];?> <a href='customer.php?id=<?php echo $order['customer'];?>'>View Details</a><br />
     <strong>Order Summary</strong><br />
     <?php
 	$orderBasket = $dbConn->query("SELECT * FROM `basket` WHERE id='".$order['basket']."' LIMIT 1");
@@ -26,7 +48,14 @@ $status = $config->getNode('orderstatus',$order['status']);
 		die("<div class='ui-state-error'><span class='ui-icon ui-icon-alert'></span>I couldn't find the basket for the selected order!</div>");
 	}
 	$orderBasket = $dbConn->fetch($orderBasket);
-	$orderBasket = unserialize($orderBasket['obj']);
+	$orderBasket = unserialize(base64_decode($orderBasket['obj']));
 	echo $orderBasket->listItems("ORDER");
 	?>
 </div>
+<script>
+function updateStatus(statusid) {
+	$('#saverTxt').html('Updating Status...').addClass('ui-state-highlight');
+	$.ajax({url:"updateStatus.php?order=<?php echo $order['id'];?>&status="+statusid,success:function(){$('#saverTxt').html("<span class='ui-icon ui-icon-circle-check'></span>Status Updated!")}});
+}
+</script>
+</body></html>
