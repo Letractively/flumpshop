309ec2842a85c081099b2c0df3c03ab0f1741f5d
flump5281 <flump5281@05db1420-0172-11df-897e-95fb7fca2173>
2010-06-14 20:29:06 +0000

    Added detailed comments and minor code changes

--- trunk/includes/Cart.class.php
+++ trunk/includes/Cart.class.php
@@ -1,14 +1,8 @@
 <?php
 
-/*
-* ====================================================================
-*  Name        : Cart.class.php
-*  Description : Provides global logic and storage for session
-*				 basket data
-*  Version     : 1.0
+/**
+*  Provides global logic and storage for session basket data
 *
-*  Copyright (c) 2009-2010 Lloyd Wallis, lloyd@theflump.com
-*  
 *  This file is part of Flumpshop.
 *
 *  Flumpshop is free software: you can redistribute it and/or modify
@@ -23,7 +17,12 @@
 *
 *  You should have received a copy of the GNU General Public License
 *  along with Flumpshop.  If not, see <http://www.gnu.org/licenses/>.
-* ====================================================================
+*
+*
+*  @Name        : Cart.class.php
+*  @Version     : 1.0
+*  @author		: Lloyd Wallis <lloyd@theflump.com>
+*  @copyright	: Copyright (c) 2009-2010, Lloyd Wallis
 */
 
 class Cart {
@@ -38,7 +37,7 @@ class Cart {
     * Cart constructor.
     * @since 1.0
     * @param $id. Optional. The ID of the Cart to load. If unset, creates a new Cart, and assigns itself to the active session.
-    * @return No return value.
+    * @return void No return value.
     */
 	function Cart($id = -1) {
 		global $dbConn;
@@ -85,6 +84,12 @@ class Cart {
 		return $this->checkPrice();
 	}
 	
+	/**
+    * Updates the total prices variables, to ensure everything is correct
+    * @since 1.0
+    * @param No arguments.
+	* @return void No return value.
+    */
 	function checkPrice() {
 		$newPrice = 0;
 		$newDelivery = 0;
@@ -97,6 +102,12 @@ class Cart {
 		$this->delivery = $newDelivery;
 	}
 	
+	/**
+    * Completely empties the Cart of all items
+    * @since 1.0
+    * @param No arguments.
+    * @return void No return value.
+    */
 	function clear() {
 		if (!$this->lock) {
 			global $dbConn;
@@ -112,6 +123,13 @@ class Cart {
 		}
 	}
 	
+	/**
+    * Locks the basket, stopping it from being edited later.
+	* Used to prevent a user from changing their order after they've started the checkout process
+    * @since 1.0
+    * @param No arguments.
+    * @return void No return value.
+    */
 	function lock() {
 		global $dbConn;
 		debug_message("Locking Basket");
@@ -119,6 +137,12 @@ class Cart {
 		$dbConn->query("UPDATE `basket` SET `locked`='1' WHERE id=".$this->id." LIMIT 1");
 	}
 	
+	/**
+    * Unlocks the basket, allowing the user to edit it again if they cancel the checkout prcess.
+    * @since 1.0
+    * @param No arguments.
+    * @return void No return value.
+    */
 	function unlock() {
 		global $dbConn;
 		debug_message("Unlocking Basket");
@@ -126,6 +150,15 @@ class Cart {
 		$dbConn->query("UPDATE `basket` SET `locked`='0' WHERE id=".$this->id." LIMIT 1");
 	}
 	
+	/**
+    * Adds an item to the basket, or increases the quantity if it is already there
+    * @since 1.0
+    * @param $id. Required. The Unique ID Number of the item to be added.
+	* @param int $stock Optional. The quantity of this item to be added. Default 1.
+	* @param float $price Optional. If set, this is the price to use for the TOTAL (price*quantity) of the item.
+	* Default -1 (Use default from DB). Used in ACP Order Form to provide customers with realtime discounts
+    * @return void No return value.
+    */
 	function addItem($id,$stock=1,$price = -1) {
 		global $dbConn;
 		$id = intval($id); //Remove zerofill
@@ -138,15 +171,18 @@ class Cart {
 							".$stock."*(SELECT price FROM `products` WHERE id=".$id."))");
 		} else {
 			//Increment current total
-			$this->items[$id]+=$stock;
-			$dbConn->query("UPDATE `basket_items`
-							SET quantity = ".$this->items[$id].",
-							sold_at = ".$this->items[$id]."*(SELECT price FROM `products` WHERE id=".$id.")
-							WHERE item_id=$id AND basket_id=".$this->id." LIMIT 1");
+			$this->changeQuantity($id,$this->items[$id]+$stock);
 		}
 		$this->change = true;
 	}
 	
+	
+	/**
+    * Removes the item with the specified ID from the basket.
+    * @since 1.0
+    * @param int $id Required. The unique ID of the item to remove.
+    * @return void No return value.
+    */
 	function removeItem($id) {
 		global $dbConn;
 		$id = intval($id); //Remove zerofill
@@ -157,6 +193,13 @@ class Cart {
 		}
 	}
 	
+	/**
+    * Sets the quantity (not increment) of an item that is already in the basket
+    * @since 1.0
+    * @param int $itemid Required. The unique ID of the item to be added.
+	* @param int $stock Required. The quantity of the item.
+    * @return void No return value.
+    */
 	function changeQuantity($itemid,$stock) {
 		global $dbConn;
 		$stock = intval($stock); //Validation. No SQL Injection for You!
@@ -169,18 +212,42 @@ class Cart {
 		$this->change = true;
 	}
 	
+	/**
+    * Get the quantity of the specified item
+    * @since 1.0
+    * @param int $itemID Required. The Unique ID of the item.
+    * @return int|bool An integer representing the quantity of the item, or false if the item has not been added
+    */
 	function getQuantity($itemID) {
 		return $this->items[$itemID];
 	}
 	
+	/**
+    * Private function. Used to increment the basket total by the specified amount.
+    * @since 1.0
+    * @param int $int Required. An integer of float value to increment the total by.
+    * @return void No return value.
+    */
 	function incTotal($int) {
 		$this->total+=$int;
 	}
 	
+	/**
+    * Private function. Used to increment the basket delivery total by the specified amount.
+    * @since 1.0
+    * @param int $int Required. An integer of float value to increment the total by.
+    * @return void No return value.
+    */
 	function incDelivery($int) {
 		$this->delivery+=$int;
 	}
 	
+	/**
+    * Returns a user-readable summary of the items in the basket.
+    * @since 1.0
+    * @param string $mode Optional. What format to output the data as. Default BASKET.
+    * @return string A HTML list of items.
+    */
 	function listItems($mode = "BASKET") {
 		global $config;
 		$mode = strtoupper($mode);
@@ -210,6 +277,13 @@ class Cart {
 		return $reply;
 	}
 	
+	/**
+    * Cart destructor. Used to save changes to the basket data in the database.
+	* Manually called by register_shutdown_function in PHP Versions < 5
+    * @since 1.0
+    * @param void No arguments.
+    * @return void No return value -- object is destroyed after function execution
+    */
 	function __destruct() {
 		global $dbConn;
 		if ($this->lock == 1) {
@@ -224,11 +298,23 @@ class Cart {
 		}
 	}
 	
-	//Getters
+	/**
+    * Returns a float value representing the current total value of the cart.
+    * @since 1.0
+    * @param void No arguments.
+    * @return float A float value representing the current total value of the cart.
+    */
 	function getTotal() {
 		return $this->total;
 	}
 	
+	/**
+    * Returns a user-readable string representing the current total value of the cart.
+    * @since 1.0
+    * @param bool $vat Optional. Whether the total should include tax. Default false.
+	* @param bool $delivery Optional. Whether the total should include delivery. Default false.
+    * @return string A string representing the current total value of the cart, to 2 decimal places.
+    */
 	function getFriendlyTotal($vat = false, $delivery = false) {
 		global $config;
 		if ($vat) $multiplier = ($config->getNode('site','vat')/100)+1; else $multiplier = 1;
@@ -239,25 +325,54 @@ class Cart {
 		}
 	}
 	
+	/**
+    * Returns a user-readable string representing the tax that will be applied to this order.
+    * @since 1.0
+    * @param void No arguments.
+    * @return string A string representing the tax that will be applied to this order.
+    */
 	function getFriendlyVAT() {
 		global $config;
 		$multiplier = ($config->getNode('site','vat')/100);
 		return number_format($this->total*$multiplier,2);
 	}
 	
+	/**
+    * Returns a user-readable string representing the delivery that will be applied to this order.
+    * @since 1.0
+    * @param void No arguments.
+    * @return string A string representing the delivery cost of this order, to 2 d.p.
+    */
 	function getFriendlyDelivery() {
 		return number_format($this->delivery,2);
 	}
 	
+	/**
+    * Returns the number of items in the cart.
+    * @since 1.0
+    * @param void No arguments.
+    * @return int The current number of unique items in the cart.
+    */
 	function getItems() {
 		return sizeof($this->items);
 	}
 	
+	/**
+    * Returns the global unique identifier of this cart.
+    * @since 1.0
+    * @param void No arguments.
+    * @return int The unique ID of this cart.
+    */
 	function getID() {
 		return $this->id;
 	}
 	
-	//Check Stock on Items
+	/**
+    * Checks whether there is sufficient stock for all of the items in the order.
+    * @since 1.0
+    * @param void No arguments.
+    * @return bool|int True if there is sufficient stock, or the ID number of the first item found with insufficient stock.
+    */
 	function checkAvail() {
 		global $_PRINTDATA;
 		$items = array_keys($this->items);
@@ -272,7 +387,13 @@ class Cart {
 		return true;
 	}
 	
-	//Temporarily mark stock as taken
+	/**
+    * Temporarily mark stock for order as used, to prevent over-ordering.
+    * @since 1.0
+    * @param int $expire Required. When this temporary lock should expire
+	* @param int $id Optional. Which item id number to lock. -1 means all items in basket. Default -1.
+    * @return bool Always returns true.
+    */
 	function holdItem($expire,$id = -1) {
 		global $dbConn, $_PRINTDATA;
 		$this->checkPrice();
@@ -297,7 +418,12 @@ class Cart {
 		return true;
 	}
 	
-	//Cancel Holds on Items
+	/**
+    * Remove temporary locks on stock in the event of a user cancelling the checkout process.
+    * @since 1.0
+    * @param int $id Optional. Which item id number to unlock. -1 means all items in basket. Default -1.
+    * @return bool Always returns true.
+    */
 	function cancelHold($id = -1) {
 		//-1 = ALL
 		if ($id == -1) {
