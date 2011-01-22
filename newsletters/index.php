147effa2d8a0951fc0446aed6b431f01abc948fe
flump5281 <flump5281@gmail.com>
2011-01-22 22:00:54 +0000

    Changed the mailer to use a .tpl layout so it can be properly table-written. Sample flumpshop theme tested in GMail, Hotmail, Yahoo! and Evolution. Been a while, so there's probably a bunch of other stuff too.

--- newsletters/index.php
+++ newsletters/index.php
@@ -2,7 +2,7 @@
 require_once "../preload.php";
 
 //Find the newsletter in the database
-$result = $dbConn->query("SELECT (body) FROM `newsletters` WHERE id=".intval($_GET['id'])." LIMIT 1");
+$result = $dbConn->query("SELECT (body) FROM `newsletters` WHERE newsletter_id=".intval($_GET['id'])." LIMIT 1");
 //Return 404 if newsletter doesn't exist
 if ($dbConn->rows($result) == 0) {
 	header("Location: ../errors/404.php");
