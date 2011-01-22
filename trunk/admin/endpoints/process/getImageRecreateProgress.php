06bd8bf455a48eaf40807f58c03818964c796266
flump5281@gmail.com <flump5281@gmail.com@05db1420-0172-11df-897e-95fb7fca2173>
2011-01-22 22:06:49 +0000

    TinyMCE Update. Newsletters

--- trunk/admin/endpoints/process/getImageRecreateProgress.php
+++
@@ -1,5 +0,0 @@
-<?php
-$requires_tier2 = true;
-require_once "../../../preload.php";
-echo "Status: ".file_get_contents($config->getNode('paths','offlineDir')."/admin_image_rebuild_status.txt");
-?>
--- trunk/admin/upgrade/pages/downloader.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once dirname(__FILE__)."/../../../preload.php";
-copy("http://flumpshop.googlecode.com/files/".$_GET['file'],$config->getNode('paths','offlineDir')."/".$_GET['file']);
-echo "Download Complete. <a href='notices.php'>Click here to continue.</a>";
-?>
