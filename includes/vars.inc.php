1ca55fe8515ba893bbf980c39b43b823ff0ed33f
flump5281 <flump5281@gmail.com>
2011-05-17 13:51:06 +0000

    Added an additional check to show an error message if the conf.txt file does not exist instead of just dying.

--- includes/vars.inc.php
+++ includes/vars.inc.php
@@ -3,7 +3,7 @@
 require_once dirname(__FILE__)."/Config.class.php";
 require_once dirname(__FILE__)."/file_get_contents.inc.php";
 //Debug Purposes Only
-$INIT_DEBUG = false;
+$INIT_DEBUG = true;
 if (!$INIT_DEBUG) error_reporting(0);
 
 //Allow use without preloader
@@ -22,6 +22,14 @@ if (!file_exists(dirname(__FILE__)."/../conf.txt")) {
 	}
 } else {
 	$confDat = file_get_contents(dirname(__FILE__)."/../conf.txt");
+	if (!file_exists($confDat)) {
+		//Configuration Data File is invisible!
+		header('HTTP/1.1 500 Internal Server Error');
+		echo '<h1>HTTP/1.1 500 Internal Server Error</h1>';
+		echo '<p>An error has occurred that has prevent the server from handling requests.</p>',
+				'<p>Error: Configuration file '.$confDat.' does not exist or is not accessible.</p>';
+		exit;
+	}
 	if (!$GLOBALS['config'] = unserialize(file_get_contents($confDat))) {
 		if (!strstr($_SERVER['REQUEST_URI'],"/admin/setup")) {
 			header("Location: ./admin/setup/");
@@ -31,4 +39,3 @@ if (!file_exists(dirname(__FILE__)."/../conf.txt")) {
 	if (!is_writable($config->getNode("paths","offlineDir")."/conf.txt")) $config->setEditable(false);
 	if (!isset($_PRINTDATA)) $_PRINTDATA = $config->getNode('server','debug');
 }
-?>
