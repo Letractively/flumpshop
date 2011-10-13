aac8b7614db5ea626513e3a01e357ec31fcb0c8e
flump5281@gmail.com <flump5281@gmail.com@05db1420-0172-11df-897e-95fb7fca2173>
2011-10-13 13:01:41 +0000

    Page_type bugfix

--- trunk/legal/terms.php
+++ trunk/legal/terms.php
@@ -1,6 +1,6 @@
 <?php
-require_once "../preload.php";
 define("PAGE_TYPE","termsConditions");
+require_once "../preload.php";
 $page_title = $config->getNode("messages","termsConditionsHeader");
 
 require_once dirname(__FILE__)."/../header.php";
@@ -12,4 +12,3 @@ echo placeholders($config->getNode("messages","termsConditions"));
 templateContent();
 
 require_once dirname(__FILE__)."/../footer.php";
-?>
