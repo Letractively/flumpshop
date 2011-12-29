24f0e2acfd4840a6d579f360a42d0bd33d1c13dc
flump5281@gmail.com <flump5281@gmail.com@05db1420-0172-11df-897e-95fb7fca2173>
2011-12-29 22:47:29 +0000

    Minor file path rejiggle (i.e. EVERYTHING HAS CHANGED)

--- trunk/captcha/ajaxVerify.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!isset($_SESSION)) session_start();
-include dirname(__FILE__)."/securimage.php";
-include dirname(__FILE__)."/../includes/json_encode.inc.php";
-$img = new Securimage();
-echo json_encode($img->check($_GET['captcha']));
-?>
--- trunk/style/cssprovider.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "../includes/vars.inc.php";
-header("Content-Type: text/css");
-
-echo file_get_contents($config->getNode('paths','offlineDir')."/themes/".$_GET['theme']."/".$_GET['sub'].".css");
-?>
