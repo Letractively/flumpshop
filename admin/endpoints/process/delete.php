a34ba18ef057f30430866b50509599c24f8954df
flump5281 <flump5281@gmail.com>
2011-12-29 22:47:29 +0000

    Minor file path rejiggle (i.e. EVERYTHING HAS CHANGED)

--- admin/endpoints/process/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-$requires_tier2 = true;
-require_once dirname(__FILE__)."/../header.php";
-
-unlink($config->getNode("paths","offlineDir")."/files/".$_GET['file']);
-echo "<div class='ui-state-highlight'><span class='ui-icon ui-icon-circle-check'></span>".$_GET['file']." has been deleted.</div>";
-include dirname(__FILE__)."/../advanced/upload.php";
-?>
