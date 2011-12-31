2835f548c21499a995030ac1b88485e440a3c758
flump5281@gmail.com <flump5281@gmail.com@05db1420-0172-11df-897e-95fb7fca2173>
2011-12-31 00:24:34 +0000

    Added framework for setup stage

--- trunk/public/admin/setup/stages/paths.php
+++ trunk/public/admin/setup/stages/paths.php
@@ -1,44 +1,34 @@
 <?php
-require_once dirname(__FILE__)."/../header.inc.php";
-?><h1>Paths and Directories</h1>
-<form action="../process/doPaths.php" method="post"><table>
-<tr>
-	<td><label for="root">Home URL</label></td>
-    <td><input type="text" name="root" id="root" value="<?php echo $_SESSION['config']->getNode('paths','root');?>" /></td>
-    <td><span class='iconbutton' onclick='$("#rootHelp").dialog("open");'></span></td>
-</tr>
-<tr>
-	<td><label for="secureRoot">Secure Home URL</label></td>
-    <td><input type="text" name="secureRoot" id="secureRoot" value="<?php echo $_SESSION['config']->getNode('paths','secureRoot');?>" /></td>
-    <td><span class='iconbutton' onclick='$("#secureRootHelp").dialog("open");'></span></td>
-</tr>
-<tr>
-	<td><label for="path">Home Directory</label></td>
-    <td><input type="text" name="path" id="path" value="<?php echo $_SESSION['config']->getNode('paths','path');?>" /></td>
-    <td><span class='iconbutton' onclick='$("#pathHelp").dialog("open");'></span></td>
-</tr>
-<tr>
-	<td><label for="offlineDir">Offline Directory</label></td>
-    <td><input type="text" name="offlineDir" id="offlineDir" value="<?php echo $_SESSION['config']->getNode('paths','offlineDir');?>" onChange="if (document.logDirFocus) $('#logDir').val($(this).val()+'/logs');" /></td>
-    <td><span class='iconbutton' onclick='$("#offlineDirHelp").dialog("open");'></span></td>
-</tr>
-<tr>
-	<td><label for="logDir">Log Directory</label></td>
-    <td><input type="text" name="logDir" id="logDir" value="<?php echo $_SESSION['config']->getNode('paths','logDir');?>" onFocus="document.logDirFocus = false;" /></td>
-    <td><span class='iconbutton' onclick='$("#logDirHelp").dialog("open");'></span></td>
-</tr>
-</table>
-<input type="submit" value="Continue" />
-</form>
-<div class="ui-helper-hidden helpDialog" id="rootHelp" title="Home URL">This is the URL of the main directory of the site. This should be automatically worked out, but check it just to be sure.</div>
-<div class="ui-helper-hidden helpDialog" id="secureRootHelp" title="Secure Home URL">This is the URL of the main directory of the site when using SSL encryption. This should be automatically worked out, but check it just to be sure.</div>
-<div class="ui-helper-hidden helpDialog" id="pathHelp" title="Home Directory">This is the physical location of the site on the server. This should be automatically worked out, but check it just to be sure.</div>
-<div class="ui-helper-hidden helpDialog" id="offlineDirHelp" title="Offline Directory">This is the physical location of a directory on the server that Flumpshop has full permissions over. It is highly recommeneded that you ensure this folder cannot be accessed from the Internet. Make sure this directory already exists, as I won't create it for you.</div>
-<div class="ui-helper-hidden helpDialog" id="logDirHelp" title="Log Directory">This is the physical location of a directory that Flumpshop has full permissions over. By default, this is a subdirectory called logs within the Offlien Direcory. Make sure this directory already exists, as I won't create it for you.</div>
-<script>
-$('.iconbutton').button({icons: {primary: 'ui-icon-help'}}).width('16px').height('16px');
-$('.helpDialog').each(function() {$(this).dialog({autoOpen: false});});
-$('input:submit').button().width('100px').css('font-size','12px');
-document.logDirFocus = true;
-</script><?php
-require_once dirname(__FILE__)."/../footer.inc.php";?>
+/**
+ *  This file is the Controller for the paths stage of the setup wizard.
+ *
+ *  This file is part of Flumpshop.
+ *
+ *  Flumpshop is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Flumpshop is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Flumpshop.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *  @Name public/admin/setup/stages/paths.php
+ *  @Version 2.00
+ *  @author Lloyd Wallis <flump5281@gmail.com>
+ *  @copyright Copyright (c) 2009-2012, Lloyd Wallis
+ *  @package Flumpshop
+ */
+
+
+require '../../../../includes/setup/header.inc.php';
+
+require '../../../../models/setup_stage_paths.inc';
+require '../../../../views/setup_stage.inc';
+
+require_once '../../../../includes/setup/footer.inc.php';
