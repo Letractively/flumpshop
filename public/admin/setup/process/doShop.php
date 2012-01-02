935017e8cac09f1b81a9e45ea7d377dedd3872a0
flump5281 <flump5281@gmail.com>
2012-01-02 04:33:23 +0000

    Updated the about, paths, database, security and shop stages of the setup wizard. Improved the CSS layout of textarea form fields, and improved the customisability of the generic stage view.

--- public/admin/setup/process/doShop.php
+++ public/admin/setup/process/doShop.php
@@ -1,12 +1,36 @@
 <?php
-require_once dirname(__FILE__)."/../header.inc.php";
+/**
+ *  This file is the Controller that process the input from the shop stage
+ * of the setup wizard. It may re-call the paths Controller if its sanity checks
+ * deem it necessary.
+ *
+ *  This file is part of Flumpshop.
+ *
+ *  Flumpshop is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Flumpshop is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Flumpshop.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *  @Name public/admin/setup/process/doShop.php
+ *  @Version 2.00
+ *  @author Lloyd Wallis <flump5281@gmail.com>
+ *  @copyright Copyright (c) 2009-2012, Lloyd Wallis
+ *  @package Flumpshop
+ */
+require_once '../../../../includes/setup/lib.inc';
 
-$_SESSION['config']->setNode("site","enabled",isset($_POST['enabled']));
-$_SESSION['config']->setNode("site","vat",$_POST['vat']);
-$_SESSION['config']->setNode("site","shopMode",isset($_POST['shopMode']));
-$_SESSION['config']->setNode("site","sendFeedback",isset($_POST['sendFeedback']));
+$tree = 'shop';
+$navigation_stage = '3.1';
+$next_stage = getNextStage(5);
 
-//Process next stage
-echo "<script>window.location = '../stages/".getNextStage(5)."'</script>";
-require_once dirname(__FILE__)."/../footer.inc.php";
-?>
+require '../../../../models/setup_stage_process_generic.inc';
+require '../../../../views/setup_stage_process_referrer.inc';
